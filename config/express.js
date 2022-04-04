bc0a64888738102884e82d53b05744dd08c79419
mellito <dayanalexismanrique@hotmail.com>
2022-04-04 11:51:09 -0500

    installing cors

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
-const express = require ('express');
-const morgan = require ('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-function configExpress(app){
+function configExpress(app) {
   app.use(express.json());
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
+  app.use(cors);
 }
 
 module.exports = configExpress;
